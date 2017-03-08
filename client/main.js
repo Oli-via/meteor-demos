3aa3e26238bf2940bbf3b9f7db7df6370c14a424
Olivia Wang <aoli.wl@alibabapictures.com>
2017-03-08 17:24:04 +0800

    init

--- client/main.js
+++ client/main.js
@@ -6,18 +6,28 @@
 
 // any js in here is automatically ran for us
 
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import ImageList from './components/image_list';
+import axios from 'axios';
 
-const App = () => {
-  return (
-    <div>
-      <ImageList>
-
-      </ImageList>
-    </div>
-  )
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { images: [] };
+  }
+  componentWillMount(){
+    axios.get("https://api.imgur.com/3/gallery/hot/viral/0")
+      .then(response => this.setState({ images: response.data.data }));
+  }
+  render(){
+    console.log(this.state.images);
+    return(
+      <div>
+        <ImageList />
+      </div>
+    );
+  }
 };
 
 // 不加这个方法的话，querySelector会在dom形成之前执行，就会报错，找不到这个dom
