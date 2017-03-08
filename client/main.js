4432b21e985a9c0da696c7043e4f697950b7bf3b
Olivia Wang <aoli.wl@alibabapictures.com>
2017-03-08 18:17:55 +0800

    init

---
+++ client/main.js
@@ -0,0 +1,15 @@
+/**
+ * Created by aliyy on 2017/3/8.
+ */
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App = () => {
+  return (
+    <div>Hello there!</div>
+  )
+}
+
+Meteor.startup(() => {
+  ReactDom.render(<App/>, document.querySelector(".container"));
+});
---
+++ server/main.js
@@ -0,0 +1,3 @@
+/**
+ * Created by aliyy on 2017/3/8.
+ */
