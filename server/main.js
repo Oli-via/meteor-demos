936ea6055bf2ad9c4d10b327ea52d909a17fe922
Olivia Wang <aoli.wl@alibabapictures.com>
2017-03-08 18:28:42 +0800

    init

--- server/main.js
+++ server/main.js
@@ -1,3 +1,24 @@
 /**
  * Created by aliyy on 2017/3/8.
  */
+import { Meteor } from 'meteor/meteor';
+import { Employees } from '../imports/collections/employee';
+import _ from 'lodash';
+import { image, helpers } from 'faker';
+
+Meteor.startup(() => {
+  const numberRecords = Employees.find({}).count();
+  if (!numberRecords){
+    // 第二个参数的function会被调用5000次，.times相当于for循环
+    _.times(5000, () => {
+      const { name, email, phone} = helpers.createCard();
+      // 等于 const name = helpers.createCard().name;
+
+      Employees.insert({
+        name, email, phone,
+        avatar: image.avatar()
+      });
+    })
+  }
+
+});
