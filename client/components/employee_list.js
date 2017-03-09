14214ff4c44cc5913ce5d655468931b1d8a28f27
Olivia Wang <aoli.wl@alibabapictures.com>
2017-03-09 10:52:57 +0800

    init

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -12,9 +12,8 @@ const EmployeeList = (props) => {
   return (
     <div>
       <div className="employee-list">
-        { props.employees.map( employee => <EmployeeDetail key={employee._id}/> ) }
+        { props.employees.map( employee => <EmployeeDetail employee={employee} key={employee._id}/> ) }
       </div>
-
     </div>
   )
 };
