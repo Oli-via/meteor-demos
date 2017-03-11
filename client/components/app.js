64cbd616fcbc1a23bd725e3b0f3b8bf6704f196d
Olivia Wang <aoli.wl@alibabapictures.com>
2017-03-11 11:40:34 +0800

    start

---
+++ client/components/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by aliyy on 2017/3/11.
+ */
+import React from 'react';
+
+export default () => {
+  return (
+    <div>Mark Bin!</div>
+  )
+}
--- client/main.js
+++ client/main.js
@@ -1,23 +1,7 @@
-/**
- * Created by aliyy on 2017/3/8.
- */
 import React from 'react';
 import ReactDom from 'react-dom';
-import Header from './components/header';
-import LinkCreate from './components/link_create';
-import { Links } from '../imports/conllections/links';
-import LinkList from './components/link_list';
+import App from './components/app';
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <LinkCreate />
-      <LinkList />
-    </div>
-  )
-};
-
-Meteor.startup(() => {
+Meteor.startup(()=>{
   ReactDom.render(<App/>, document.querySelector(".container"));
 });
