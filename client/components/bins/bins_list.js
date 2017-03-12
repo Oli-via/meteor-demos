4cc7eea4f92c66bc2f6ecb93f11edf63371fafc5
Olivia Wang <aoli.wl@alibabapictures.com>
2017-03-12 09:22:26 +0800

    bin remove

--- client/components/bins/bins_list.js
+++ client/components/bins/bins_list.js
@@ -7,11 +7,25 @@ import { createContainer } from 'meteor/react-meteor-data';
 import Bins from '../../../imports/collections/bins';
 
 class BinsList extends Component {
+  onBinRemove (bin){
+    Meteor.call('bins.remove', bin);
+  }
   renderList(){
     return this.props.bins.map( bin => {
       return (
         <li className="list-group-item" key={bin._id}>
           Bin {bin._id}
+          <span className="pull-right">
+            {/*onClick={()=>{this.onBinRemove(bin)}}*/}
+            {/*这样写会在点击的时候才触发onBinRemove*/}
+            {/*如果写成这样：onClick={this.onBinRemove(bin)}*/}
+            {/*则会在渲染的时候立即触发，但是需要不同的bin，所以写成函数的形式*/}
+            <button
+              className="btn btn-danger"
+              onClick={()=>{this.onBinRemove(bin)}}>
+              Remove
+            </button>
+          </span>
         </li>
       )
     })
