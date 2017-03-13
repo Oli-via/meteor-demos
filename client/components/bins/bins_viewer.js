ccadda3f765d03b6f1f367f6c7d17ba060de17bc
Olivia Wang <aoli.wl@alibabapictures.com>
2017-03-13 11:07:41 +0800

    bins viewer

--- client/components/bins/bins_viewer.js
+++ client/components/bins/bins_viewer.js
@@ -1,3 +1,25 @@
 /**
  * Created by Olivia on 2017/3/13.
  */
+import React, { Component } from 'react';
+import { markdown } from 'markdown';
+
+class BinsViewer extends Component{
+  render (){
+    if (!this.props.bin){
+      return <div></div>;
+    }
+    // rawHTML是string
+    const rawHTML = markdown.toHTML(this.props.bin.content);
+
+    return (
+      <div className="col-xs-4">
+        <h5>Output</h5>
+        {/* HTML转译 xxs attack */}
+        <div dangerouslySetInnerHTML={{  __html: rawHTML }}></div>
+      </div>
+    )
+  }
+}
+
+export default BinsViewer;
